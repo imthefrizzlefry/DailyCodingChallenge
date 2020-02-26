92981cee682629964f075795df9ca68e4a663d16
Farnell <sfarnell@kingcounty.gov>
2020-02-26 10:21:43 -0800

    updating set

--- easy/findSumOfK.py
+++ easy/findSumOfK.py
@@ -1,9 +1,7 @@
-from sets import Set
-
 def FindSumOfK(s, k):
     ''' return whether any two numbers from the list s add up to k '''
     #create an empty set of compliments needed for reach K
-    compList = Set()
+    compList = set()
     #for each num a in s, see if k-a in in the compliment set
     for a in s:
         # if found, return true otherwise add the compliment
