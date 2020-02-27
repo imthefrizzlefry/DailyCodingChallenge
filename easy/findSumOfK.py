e94aa55a1f67413f44f54fcde3e481bb93b3dbf4
Farnell <sfarnell@kingcounty.gov>
2020-02-26 16:13:46 -0800

    Adding Tests and Correcting Solution To Work

--- easy/findSumOfK.py
+++ easy/findSumOfK.py
@@ -1,14 +1,35 @@
-def FindSumOfK(s, k):
+import logging
+
+def findSumOfK(s, k):
     ''' return whether any two numbers from the list s add up to k '''
+    logging.debug(s)
+    logging.debug(k)
     #create an empty set of compliments needed for reach K
-    compList = set()
+    compList = set([])
+    logging.debug(compList)
+
     #for each num a in s, see if k-a in in the compliment set
     for a in s:
         # if found, return true otherwise add the compliment
-        comp = k-a
-        if comp in compList:
+        comp = k-a   
+        logging.debug(comp)
+
+        if a in compList:
+            logging.debug("Found Compliment")
             return True
         else:
+            logging.debug("Adding Compliment To List")
             compList.add(comp)
-
+            
+    logging.debug("Compliment Not Found")
     return False
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    s = [10, 15, 3, 7]
+    k = 17
+
+    findSumOfK(s, k)
+
+    
--- easy/test_findSumOfK.py
+++ easy/test_findSumOfK.py
@@ -1,9 +1,27 @@
 import unittest
-from findSumOfK import FindSumOfK
+from findSumOfK import findSumOfK
 
 class findSumOfKTests(unittest.TestCase):
-    def test_findSumOfTests_ProvidedExample(self):
+    def test_findSumOfTests_Example(self):
         s = [10, 15, 3, 7]
         k = 17
 
-        self.assertTrue(FindSumOfK(s, k))
+        self.assertTrue(findSumOfK(s, k))
+
+    def test_findSumOfTests_ReturnsFalse(self):
+        s = [10, 15, 3, 7]
+        k = 20
+
+        self.assertFalse(findSumOfK(s, k))
+
+    def test_findSumOfTests_NegativeKValue(self):
+        s = [10, -15, 3, 7]
+        k = -12
+
+        self.assertTrue(findSumOfK(s, k))
+
+    def test_findSumOfTests_NegativeNumbersInSet(self):
+        s = [10, 15, -3, 7]
+        k = 12
+
+        self.assertTrue(findSumOfK(s, k))
